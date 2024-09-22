2727d7dd6cf6b7e8f7b0a463990d48e5264efbbd
Lucas Henrique <167331612+Devlukinha@users.noreply.github.com>
2024-09-22 18:17:33 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button");
+
+botao.addEventListener('click', alerta);
+
+function alerta(){
+    alert("Desculpe mais no momento o formulário ainda não funciona!! Ainda tô aprendendo Javascript 😂");
+}
